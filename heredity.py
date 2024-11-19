6aac8a6d24b4c3fa9533c812a5e5dda11df3731e
João Oliveira <joaopaulocamara1@hotmail.com>
2024-11-19 15:43:40 +0000

    normalize

--- heredity.py
+++ heredity.py
@@ -205,13 +205,21 @@ def update(probabilities, one_gene, two_genes, have_trait, p):
         # Update trait probabilities
         probabilities[person]["trait"][has_trait] += p
 
-
 def normalize(probabilities):
     """
     Update `probabilities` such that each probability distribution
     is normalized (i.e., sums to 1, with relative proportions the same).
     """
-    raise NotImplementedError
+    for person in probabilities:
+        # Normalize gene probabilities
+        gene_total = sum(probabilities[person]["gene"].values())
+        for gene in probabilities[person]["gene"]:
+            probabilities[person]["gene"][gene] /= gene_total
+
+        # Normalize trait probabilities
+        trait_total = sum(probabilities[person]["trait"].values())
+        for trait in probabilities[person]["trait"]:
+            probabilities[person]["trait"][trait] /= trait_total
 
 
 if __name__ == "__main__":
